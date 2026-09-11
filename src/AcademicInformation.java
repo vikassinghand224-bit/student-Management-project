12df7606db302f19fb3fa12ac1fc96ac2aad5cb2
vikassinghand224-bit <vikassinghand224@gmail.com>
2026-09-11 14:27:19 +0530

    progess

--- src/AcademicInformation.java
+++ src/AcademicInformation.java
@@ -1,12 +1,19 @@
 package src;
 
 public class AcademicInformation {
+    private String studentId;
     private int addmissionYear;
     private CourseAndSemester course;
+    
 
-    public AcademicInformation(int addmissionYear, CourseAndSemester courseAndSemester) {
+    public AcademicInformation(String studentId, int addmissionYear, CourseAndSemester courseAndSemester) {
+        this.studentId = studentId;
         this.addmissionYear = addmissionYear;
         this.course = courseAndSemester;
+    }
+
+    public String getStudentId() {
+        return studentId;
     }
 
     public int getAddmissionYear() {
--- src/student.java
+++ src/student.java
@@ -1,5 +1,21 @@
 package src;
 
-public class student {
+public class Student {
+    private PersonalInformation personalInfo;
+    private AcademicInformation academicInfo;
+
+    public Student(PersonalInformation personalInfo, AcademicInformation academicInfo) {
+        this.personalInfo = personalInfo;
+        this.academicInfo = academicInfo;
+         
+    }
+
+    public PersonalInformation getPersonalInfo() {
+        return personalInfo;
+    }
+
+    public AcademicInformation getAcademicInfo() {
+        return academicInfo;
+    }
     
 }
