2e4f9ff9a8319b69dd0818570ad30a610ff7719f
vikassinghand224-bit <vikassinghand224@gmail.com>
2026-09-11 15:06:07 +0530

    progrees

---
+++ src/Marks.java
@@ -0,0 +1,25 @@
+package src;
+
+public class Marks {
+    private String studentId;
+    private String subjectCode;
+    private int marks;
+
+    public Marks(String studentId, String subjectCode, int marks) {
+        this.studentId = studentId;
+        this.subjectCode = subjectCode;
+        this.marks = marks;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+}
--- src/Subject.java
+++ src/Subject.java
@@ -1,6 +1,33 @@
 package src;
 
 public class Subject {
-    
+    private String subjectName;
+    private String subjectCode;
+    private int semester;
+    private CourseAndSemester course;
+
+    public Subject(String subjectName, String subjectCode, int semester, CourseAndSemester course) {
+        this.subjectName = subjectName;
+        this.subjectCode = subjectCode;
+        this.semester = semester;
+        this.course = course;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public CourseAndSemester getCourse() {
+        return course;
+    }
+
     
 }
