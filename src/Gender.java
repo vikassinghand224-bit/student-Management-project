fb9dbe5c85742048e07d042ecb90536aa1f6be01
vikassinghand224-bit <vikassinghand224@gmail.com>
2026-09-12 11:47:12 +0530

    progress

--- src/Gender.java
+++ src/Gender.java
@@ -1,5 +1,14 @@
 package src;
 
 public enum Gender {
-    MALE, FEMALE, OTHER
+    MALE, FEMALE, OTHER;
+
+    public static Gender fromString(String gender) {
+        for (Gender g : Gender.values()) {
+            if (g.name().equalsIgnoreCase(gender)) {
+                return g;
+            }
+        }
+        throw new IllegalArgumentException("Invalid gender: " + gender);
+    }
 }
--- src/personalInformation.java
+++ src/personalInformation.java
@@ -10,6 +10,7 @@ public class PersonalInformation {
     private String fatherName;
     private int fatherPhoneNumber;
     private Qualification qualification;
+    
 
     public PersonalInformation(String name, String dob, Gender gender, String address, String phoneNumber, String email, String fatherName, int fatherPhoneNumber, Qualification qualification) {
         this.name = name;
@@ -22,4 +23,32 @@ public class PersonalInformation {
         this.fatherPhoneNumber = fatherPhoneNumber;
         this.qualification = qualification;
     }
+    public String getName() {
+        return name;
+    }
+    public String getDob() {
+        return dob;
+    }
+    public Gender getGender() {
+        return gender;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getFatherName() {
+        return fatherName;
+    }
+    public int getFatherPhoneNumber() {
+        return fatherPhoneNumber;
+    }
+    public Qualification getQualification() {
+        return qualification;
+    }
+    
 }
